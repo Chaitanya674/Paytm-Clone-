cf20d3a3fb13b27d6ee620bca389fb7e4836fb70
Chaitanya674 <chaitanyasharma674@gmail.com>
2024-01-28 16:26:03 +0530

    Paytm Clone

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,69 @@ const mongoose = require('mongoose');
 const PORT = 3000;
 const app = express();
 
+//Local imports
+const { Login_Register } =  require('./zod_configure/login_Register_conf');
+const { User } = require('./db_schema/user');
+
 //middlewares
 app.use(express.json());
 app.use(cors());
 
 app.post('/signin' , async function(req , res) {
-    
+    const userCredential = req.body;
+    const parseduserCredential = Login_Register.safeParse(userCredential);
+    if(!parseduserCredential.success){
+        res.status(411).json({
+            msg : 'something wrong with input',
+        });
+        return;
+    }
+
+    const user = await User.findOne({
+        name : userCredential.name,
+        lastname : userCredential.lastname,
+        password : userCredential.password,
+    }).then(() => {
+        if(user){
+            return res.status(200).json({
+                msg : 'user can login',
+            })
+        }else{
+            return res.status(411).json({
+                msg: 'invalid user credentials'
+            })
+        }
+    }).catch((error) => {
+        console.log(error);
+    })
 })
 
 app.post('/signup' , async function(req , res) {
-    
+    const userCredential = req.body;
+    const parseduserCredential = Login_Register.safeParse(userCredential);
+    if(!parseduserCredential.success){
+        res.status(411).json({
+            msg : 'something wrong with input',
+        });
+        return;
+    }
+    const user = await User.create({
+            name : userCredential.name,
+            lastname : userCredential.lastname,
+            password : userCredential.password,
+        }).then((res) => {
+            if(user && res.status == 200){
+                return res.status(200).json({
+                    msg : 'user created',
+                })
+            }else{
+                return res.status(411).json({
+                    msg: 'invalid Input'
+                })
+            }
+        }).catch((error) => {
+            console.log(error);
+    })
 })
 
 mongoose.connect(process.env.MONGODB_URI)
