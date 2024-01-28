2e888fe74337c8b4a623252971f54d21e2ffeb2d
Chaitanya674 <chaitanyasharma674@gmail.com>
2024-01-28 15:59:41 +0530

    Paytm Clone

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,33 @@
+require('dotenv').config()
 const express = require("express");
+const mongoose = require('mongoose');
+const zod = require('zod');
+const PORT = 3000;
+const app = express();
 
+//middlewares
+app.use(express.json());
+app.use(cors());
 
+app.post('/signin' , async function(req , res) {
+
+})
+
+app.post('/signup' , async function(req , res) {
+    
+})
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("connected to the database")
+        app.listen(PORT , function(err){
+            if(err){
+                console.log(err);
+            }else{
+                console.log('Server listening on Port' , PORT);
+            }
+        });
+    })
+    .catch((error) =>{ 
+        console.log(error);
+    })
